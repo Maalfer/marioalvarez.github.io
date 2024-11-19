eb938f3e6b2f56e134b84638d4e8a18f91eb5f77
Mario <marioalvarezfdz@gmail.com>
2024-11-19 18:51:15 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,20 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        super().end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.end_headers()
+
+if __name__ == '__main__':
+    port = 5050
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, CORSRequestHandler)
+
+    print(f"Server running on port: {port}")
+    httpd.serve_forever()
